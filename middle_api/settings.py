a358b43ab820231d412fb9ec5fecdab1de55597c
Dima Ulasovets <d.ulasovets@gmail.com>
2022-04-05 08:21:29 +0300

    add allowed hosts

--- middle_api/settings.py
+++ middle_api/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+cjo@zook+5h4_cqiw@qws^9hkh7f#bh9n(r%^9!slm(bh@kdw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
